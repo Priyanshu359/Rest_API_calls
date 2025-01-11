6354e217c9ee8a14ee0d081411a945ae049ec952
Priyanshu Sinha <piyushsinha675@gmail.com>
2025-01-12 02:06:02 +0530

    add user

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import "package:flutter/material.dart";
 import 'package:http/http.dart' as http;
-// import 'package:http/http.dart%20';
+import 'package:rest_api_tutorial/Model/user.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,45 +19,56 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Rest API call"),
         centerTitle: true,
         backgroundColor: Colors.blue,
+        elevation: 10,
+        shadowColor: Colors.black,
       ),
       body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (context, index){
-          final user = users[index];
-          final name = user['name']['first'];
-          final email = user['email'];
-          final imageUrl = user['picture']['thumbnail'];
-          return ListTile(
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.network(imageUrl),
-            ),
-            title: Text(name.toString()),
-            subtitle: Text(email),
-          );
-        }
-      ),
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            final email = user.email;
+            //final color = user.gender =='male' ? Colors.blue : Colors.green;
+            return ListTile(
+              title: Text(user.name.first),
+              subtitle: Text(user.phone),
+              //tileColor: color,
+            );
+          }),
       floatingActionButton: FloatingActionButton(
-        onPressed: fetchUsers,
-        backgroundColor: Colors.blue,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        )
-      ),
+          onPressed: fetchUsers,
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          )),
     );
-  
   }
-  void fetchUsers() async{
+
+  void fetchUsers() async {
     print("FetchUsers called");
     const url = "https://randomuser.me/api/?results=100";
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
     final json = jsonDecode(body);
+    final results = json['results'] as List<dynamic>;
+    final transformed = results.map((e) {
+      final name = UserName(
+        title: e['name']['title'], 
+        first: e['name']['first'], 
+        last: e['name']['last'],
+      );
+      return User(
+        name: name,
+        cell: e['cell'],
+        email: e['email'],
+        gender: e['gender'],
+        nat: e['nat'],
+        phone: e['phone'],
+      );
+    }).toList();
     setState(() {
-      users = json['results'];
+      users = transformed;
     });
     print("fetchUsers completed");
   }
 }
-
